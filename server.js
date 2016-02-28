a58a11168c8c2d121a0017c4b2e9bcec6ab910a3
Henry Pitcairn <735tesla@gmail.com>
2016-02-28 15:47:30 -0500

    stuff

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var session = require('express-session')
-var sqlite3 = require('sqlite3').verbose();
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('sqlite://database');
+var session = require('express-session');
+Sequelize = require('sequelize');
+sequelize = new Sequelize('sqlite://sys.db');
 sequelize.sync();
 
 var Client = require('./app/models/client');
@@ -27,27 +26,68 @@ var port = process.env.PORT || 8080;
 var router = express.Router();
 
 router.use(function(req, res, next) {
-    if (req.session.auth === true) {
-      next();
-    } else {
-      res.send(JSON.stringify({
-        success: false,
-        error: 'Not authenticated'
-      }));
-      res.end();
-    }
+  if (req.session.auth === true || req.url.match('^/(authenticate)(/|)$')) {
+    next();
+  } else {
+    req.session.auth = false;
+    res.send(JSON.stringify({
+      success: false,
+      error: 'Not authenticated'
+    }));
+    res.end();
+  }
 });
 
 router.route('/authenticate')
   .post(function(req, res) {
-    res.send(JSON.stringify({
-      success: true
+    var resp = {};
+    if (req.session.auth !== true) {
+      var data = req.body;
+      if (data.uuid !== undefined) {
+        if (data.uuid.match('[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}')) {
+          req.session.auth = true;
+          var that = this;
+          Client.findOrCreate({
+            where: {
+              uuid: data.uuid
+            }
+          }).spread(function(client) {
+            // Do something with client
+          });
+          resp = {
+            success: true,
+          };
+        } else {
+          resp = {
+            success: false,
+            error: 'Invalid UUID',
+            uuid: data.uuid
+          };
+        }
+      } else {
+        resp = {
+          success: false,
+          error: 'Missing UUID'
+        };
+      }
+    } else {
+      resp = {
+        success: true,
+        error: 'Already authenticated'
+      };
+    }
+    res.end(JSON.stringify(resp));
+  });
+
+router.route('/test')
+  .get(function(req, res) {
+    res.end(JSON.stringify({
+      success: true,
+      message: 'Hello World!'
     }));
   });
 
-
 app.use('/api', router);
-
 
 app.listen(port);
 console.log('Server listening on port ' + port);
